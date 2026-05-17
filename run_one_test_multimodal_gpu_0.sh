be8934ab01a7a52d82577ce883af8e6ec5bacbc5
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2026-05-17 14:47:55 -0400

    feat: update test script to include additional model options and migrate summary generation to a new file

--- run_one_test_multimodal_gpu_0.sh
+++ run_one_test_multimodal_gpu_0.sh
@@ -2,6 +2,6 @@ source .venv/bin/activate
 # Força o PyTorch a gerenciar a memória sem fragmentar (Vital para a EfficientNet e ResNet)
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export CUDA_LAUNCH_BLOCKING=1
-PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=1 poetry run python src/run_test_multimodal.py --classes microscopy_parasite_babesia microscopy_parasite_plasmodium microscopy_parasite_trichomonad microscopy_parasite_leishmania microscopy_parasite_rbcs microscopy_parasite_trypanosome microscopy_parasite_leukocyte microscopy_parasite_toxoplasma --nets openai/clip-vit-base-patch16 --build-index
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=1 poetry run python src/run_test_multimodal.py --classes microscopy_parasite_babesia microscopy_parasite_plasmodium microscopy_parasite_trichomonad microscopy_parasite_leishmania microscopy_parasite_rbcs microscopy_parasite_trypanosome microscopy_parasite_leukocyte microscopy_parasite_toxoplasma --nets openai/clip-vit-base-patch16 google/siglip-base-patch16-224 --build-index
 
 echo "Teste concluído. Verifique os resultados e ajuste os parâmetros conforme necessário."
